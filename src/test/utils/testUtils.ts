6d1768e55e99a296e4fab37bfa0ebef9926feda4
Tenma <tenma@osxt.com>
2025-07-18 17:44:17 +0200

    add tests

---
+++ src/test/utils/testUtils.ts
@@ -0,0 +1,15 @@
+import { QueryClient } from "@tanstack/react-query";
+
+// Créer un QueryClient pour les tests
+export const createTestQueryClient = () =>
+  new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: false,
+        gcTime: 0,
+      },
+      mutations: {
+        retry: false,
+      },
+    },
+  });
