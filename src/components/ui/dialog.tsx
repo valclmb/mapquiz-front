b2ebd850ba842eb7bff79eb5a59a70a2441f2d64
Tenma <tenma@osxt.com>
2025-07-25 20:19:48 +0200

    fix animation

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -59,7 +59,7 @@ function DialogContent({
         data-slot="dialog-content"
         aria-describedby="dialog-title"
         className={cn(
-          "bg-background data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 fixed top-[50%] left-[50%] z-50 grid w-full max-w-[calc(100%-2rem)] translate-x-[-50%] translate-y-[-50%] gap-4 rounded-4xl border p-8 shadow-lg duration-200 sm:max-w-lg",
+          "bg-background data-[state=open]:animate-slide-in-from-top data-[state=closed]:animate-slide-out-to-bottom data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 fixed top-[50%] left-[50%] z-50 grid w-full max-w-[calc(100%-2rem)] translate-x-[-50%] translate-y-[-50%] gap-4 rounded-4xl border p-8 shadow-lg duration-200 sm:max-w-lg",
           className
         )}
         {...props}
